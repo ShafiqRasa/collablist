d9255a808615c3147a98c3f037a7a51fb672165e
Shafiq Salehi <rasashafiq@gmail.com>
2023-12-03 22:34:42 +0100

    issue of overwritting is gonna be solved by blocking the input boxes for other users, we can tracke that which input box is focused by whom using storing id of cell in the users computer on localstorage. that is why no longer concurrent users overwrite to my desired input box

--- app/components/editable-cell/editable-cell.component.tsx
+++ app/components/editable-cell/editable-cell.component.tsx
@@ -6,9 +6,19 @@ type EditableCellProps = {
   cell: asyncItemType;
 };
 
-const EditableCell: React.FC<EditableCellProps> = ({ cell: { id, value } }) => {
+const EditableCell: React.FC<EditableCellProps> = ({
+  cell: { id, value, focus },
+}) => {
   const { handleCellChange } = useCellContextProvider();
   const [valueUnderEdit, setValueUnderEdit] = useState<string>(value);
+
+  /** find the user who focused the input box, to style input accordingly */
+  const isFocused =
+    focus && id.toString() === localStorage.getItem('focusedId');
+
+  /** the focused inputs will be blocked for every concurrent users, except whom focused indeed */
+  const shouldBlocked =
+    focus && id.toString() !== localStorage.getItem('focusedId');
 
   useEffect(() => {
     setValueUnderEdit(value);
@@ -18,19 +28,28 @@ const EditableCell: React.FC<EditableCellProps> = ({ cell: { id, value } }) => {
     setValueUnderEdit(e.target.value);
   };
   const handleBlur = () => {
-    if (valueUnderEdit !== value) {
-      handleCellChange({ id, value: valueUnderEdit, focus: false });
-    }
+    handleCellChange({ id, value: valueUnderEdit, focus: false });
+    localStorage.setItem('focusedId', '0');
+  };
+
+  const handleFocus = () => {
+    handleCellChange({ id: id, value: valueUnderEdit, focus: true });
+    localStorage.setItem('focusedId', id.toString());
   };
 
   return (
-    <input
-      type="text"
-      className="editable-cell-input m-2"
-      value={valueUnderEdit}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
+    <div className="flex">
+      <input
+        type="text"
+        className="editable-cell-input m-2"
+        disabled={shouldBlocked}
+        value={valueUnderEdit}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+      />
+      <p>{isFocused && 'typing...'}</p>
+    </div>
   );
 };
 
--- app/components/vertical-list/verticle-list.component.tsx
+++ app/components/vertical-list/verticle-list.component.tsx
@@ -12,7 +12,7 @@ const VerticalList: React.FC = () => {
   return (
     <div>
       {cells.map(({ id, value, focus }, position) => (
-        <div key={`key-${id}-${position}`}>
+        <div key={`key-${id}-${position}`} className="flex justify-start">
           <EditableCell cell={{ id, value, focus }} />
           <button
             className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-1 text-center m-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
